85be36e5b3763e8ff81d9e989296918a7d9f1a6d
WWJ0720 <1920698385@qq.com>
2021-03-27 19:45:08 +0800

    update

--- src/main/java/com/team/ms/controller/MessageController.java
+++ src/main/java/com/team/ms/controller/MessageController.java
@@ -1,19 +1,23 @@
 package com.team.ms.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.team.ms.pojo.Forum;
 import com.team.ms.pojo.Message;
 import com.team.ms.pojo.Result;
 import com.team.ms.service.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
 public class MessageController {
     @Autowired
     MessageService messageService;
+
     @RequestMapping("/insertMessage")
-    public void insertMessage(@RequestBody Message message){
+    public  String insertMessage(@RequestBody Message message){
         messageService.insertMessage(message);
         return JSON.toJSONString(Result.successResult());
     }
